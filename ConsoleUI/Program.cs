6e69cf4a35c15fad12114619b572ae567d04b2ed
selinkarabel <selinkarabel@gmail.com>
2021-02-08 05:49:42 +0300

    -EfEntityRepositoryBase methods are added -EfBrandDal, EfCarDal, EfColorDal are updated -Brand and Color are added into Business layer -CRUD operations are tested for Brand, Car and Color

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,60 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //CarTest();
+            //BrandTest();
+            //ColorTest();
+
+        }
+
+        private static void ColorTest()
+        {
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+            Console.WriteLine("List of colors");
+            foreach (var color in colorManager.GetAll())
+            {
+                Console.WriteLine("Color Id:" + color.ColorId + " Color Name:" + color.ColorName);
+            }
+
+            Console.WriteLine("The color which has id number 1");
+            Console.WriteLine(colorManager.GetById(1).ColorName);
+
+            //Console.WriteLine("Adding a new color");
+            //colorManager.Add(new Color { ColorName = "Blue" });
+
+            //Console.WriteLine("Update a color");
+            //colorManager.Update(new Color { ColorId = 1002, ColorName = "Pink" });
+
+            //Console.WriteLine("Delete a color");
+            //colorManager.Delete(new Color { ColorId = 1002 });
+        }
+
+        private static void BrandTest()
+        {
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            Console.WriteLine("List of brands");
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine("Brand id:" + brand.BrandId + " Brand Name:" + brand.BrandName);
+            }
+
+            Console.WriteLine("The brand which has id number 3");
+            Console.WriteLine(brandManager.GetById(3).BrandName);
+
+            //Console.WriteLine("Adding new brand");
+            //Brand newBrand = new Brand();
+            //newBrand.BrandName = "Fords";
+            //brandManager.Add(newBrand);
+
+            //Console.WriteLine("Update a brand");
+            //brandManager.Update(new Brand { BrandId = 1002, BrandName = "Ford" });
+
+            //Console.WriteLine("Delete a brand");
+            //brandManager.Delete(new Brand { BrandId = 1002});
+        }
+
+        private static void CarTest()
+        {
             CarManager carManager = new CarManager(new EfCarDal());
 
             Console.WriteLine("List of the car that color id is 1");
@@ -18,9 +72,9 @@ namespace ConsoleUI
                 Console.WriteLine(car.CarName);
             }
 
-            Console.WriteLine("The car which has id number as 3");
+            Console.WriteLine("The car which has id number 3");
             Console.WriteLine((carManager.GetById(3)).CarName);
-            
+
 
 
             Console.WriteLine("List of the car that brand id is 3");
@@ -29,14 +83,18 @@ namespace ConsoleUI
                 Console.WriteLine(car.CarName);
             }
 
+            Console.WriteLine("List of cars");
+            foreach(var car in carManager.GetAll())
+            {
+                Console.WriteLine("Car Id:" + car.CarId + " CarName:"  + car.CarName + " Car Description:" + car.Description);
+            }
+
             Console.WriteLine("\n");
-            carManager.Add(new Car { BrandId = 3, ColorId = 3, DailyPrice = 00, ModelYear = 2021, Description = "New Hybrid", CarName= "5" });
+            carManager.Add(new Car { BrandId = 3, ColorId = 3, DailyPrice = 00, ModelYear = 2021, Description = "New Hybrid", CarName = "5" });
 
             carManager.Update(new Car { CarId = 6, BrandId = 3, ColorId = 3, DailyPrice = 125, ModelYear = 2014, Description = "Manuel S", CarName = "Car4" });
 
             // carManager.Delete(new Car { CarId = 7 });
-
-
         }
     }
 }
--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -9,73 +10,25 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfCarDal : ICarDal
+    public class EfCarDal : EfEntityRepositoryBase<Car,RentACarContext>,ICarDal
     {
-        public void Add(Car entity)
-        {
-            using (RentACarContext context=new RentACarContext())
-            {
-                var addedEntity = context.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                context.SaveChanges();
 
-                //another way
-                //context.Cars.Add(entity);
-                //context.SaveChanges();
-            }
-        }
+        //another way for Car Add but it is not generic
+        //context.Cars.Add(entity);
+        //context.SaveChanges();
 
-        public void Delete(Car entity)
-        {
-            using (RentACarContext context = new RentACarContext())
-            {
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
+        //another way for Car Delete but it is not generic
+        //context.Cars.Remove(context.Cars.SingleOrDefault(p => p.CarId == entity.CarId));
+        //context.SaveChanges();
 
-                //another way
-                //context.Cars.Remove(context.Cars.SingleOrDefault(p => p.CarId == entity.CarId));
-                //context.SaveChanges();
-            }
-        }
-
-        public Car Get(Expression<Func<Car, bool>> filter)
-        {
-            using (RentACarContext context = new RentACarContext())
-            {
-                return context.Set<Car>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
-        {
-            using (RentACarContext context = new RentACarContext())
-            {
-                return filter == null
-                    ? context.Set<Car>().ToList()
-                    : context.Set<Car>().Where(filter).ToList();
-            }
-        }
-
-        public void Update(Car entity)
-        {
-            using (RentACarContext context = new RentACarContext())
-            {
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-
-                //another way
-                //var carToUpdate = context.Cars.SingleOrDefault(p=>p.CarId==entity.CarId);
-                //carToUpdate.ColorId = entity.ColorId;
-                //carToUpdate.BrandId = entity.BrandId;
-                //carToUpdate.DailyPrice = entity.DailyPrice;
-                //carToUpdate.ModelYear = entity.ModelYear;
-                //carToUpdate.Description = entity.Description;
-                //carToUpdate.CarName = entity.CarName;
-
-                //context.SaveChanges();
-            }
-        }
+        //another way for Car update but it is not generic
+        //var carToUpdate = context.Cars.SingleOrDefault(p=>p.CarId==entity.CarId);
+        //carToUpdate.ColorId = entity.ColorId;
+        //carToUpdate.BrandId = entity.BrandId;
+        //carToUpdate.DailyPrice = entity.DailyPrice;
+        //carToUpdate.ModelYear = entity.ModelYear;
+        //carToUpdate.Description = entity.Description;
+        //carToUpdate.CarName = entity.CarName;
+        //context.SaveChanges();
     }
 }
