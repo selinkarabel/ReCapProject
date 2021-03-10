58a0fa3c3fb4a51993b45d61e9fd9c91030bc7db
selinkarabel <selinkarabel@gmail.com>
2021-03-10 20:59:34 +0300

    -

--- Business/BusinessAspects/Autofac/SecuredOperation.cs
+++ Business/BusinessAspects/Autofac/SecuredOperation.cs
@@ -34,7 +34,7 @@ namespace Business.BusinessAspects.Autofac
                     return;
                 }
             }
-            throw new Exception(Messages.AuthorizationDenied);
+            throw new Exception(Messages.AuthorizationDenied); 
         }
     }
 }
